7317c1a7ef7d0c28c754b8e6d395fc69e72c490f
Alex_Beng <599267065@qq.com>
2018-11-18 19:20:55 +0800

    assignment 1-1 done

---
+++ assignment_1/source/1-1.cpp
@@ -0,0 +1,16 @@
+/**
+ * OpenCV's hello world code
+ * Author: Alex Beng
+*/
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+
+int main(int argc, char const *argv[]) {
+    cv::Mat image(200, 750, CV_8UC1, cv::Scalar(0));
+    cv::putText(image, "Hello World !", cv::Point(75,150), 0, 3, cv::Scalar(255), 2);
+    cv::imshow("hello", image);
+    cv::waitKey(0);
+    return 0;
+}
